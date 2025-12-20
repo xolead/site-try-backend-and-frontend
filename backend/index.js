029a097ce42705b23ecb602c90cc3ed08961ece6
xolead <imcsasa@mail.ru>
2025-12-20 23:08:29 +0300

    start factoring frontend. Need развернуть сервер для фронта

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const PORT = 8080;
+const path = require('path');
 const productRouter = require('./database/routes/product.routes');
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
 
+app.use('/components', express.static(path.join(__dirname, 'components')));
+app.use('/pages', express.static(path.join(__dirname, 'pages')));
+const staticPath = path.join(__dirname, '../frontend/src/pages');
+
+app.use(express.static(staticPath));
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  const indexPath = path.join(__dirname, '../frontend/src/pages', 'index.html');
+  console.log('Путь к index.html:', indexPath);
+  res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
