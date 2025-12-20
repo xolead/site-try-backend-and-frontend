a3a5d8164778d505f692004a00f1bd048c9f684c
xolead <imcsasa@mail.ru>
2025-12-20 05:55:06 +0300

    backend done, start frontend

--- frontend/src/request/product.requests.js
+++ frontend/src/request/product.requests.js
@@ -13,3 +13,40 @@ async function getProduct(id) {
   const result = await responce.json();
   return result;
 }
+
+async function createProduct(name, description, price, quantity, img) {
+  await fetch('/api/product', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: name,
+      description: description,
+      price: price,
+      quantity: quantity,
+      img: img,
+    }),
+  });
+}
+
+async function deleteProduct(id) {
+  await fetch('/api/product/' + id, {
+    method: 'DELETE',
+  });
+}
+
+async function updateProduct(id, name, description, price, quantity, img) {
+  await fetch('/api/product', {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      id: id,
+      name: name,
+      description: description,
+      price: price,
+      quantity: quantity,
+      img: img,
+    }),
+  });
+}
+
+export { createProduct, getProduct, getProducts, deleteProduct, updateProduct };
